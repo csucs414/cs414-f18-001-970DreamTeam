b71df183b0d2722fa7854da1fd9cc175080d5c54
Alabastor05 <46968048+Alabastor05@users.noreply.github.com>
2019-10-13 10:14:39 -0600

    Add files via upload

---
+++ server/src/test/java/server/ServerTest.java
@@ -0,0 +1,14 @@
+package server;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ServerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
