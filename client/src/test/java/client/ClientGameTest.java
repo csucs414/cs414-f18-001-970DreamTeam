24d19b6efee61c3b3b9a5bff39d9650b5c385084
Adan Soto <asoto98@rams.colostate.edu>
2019-11-02 22:25:36 -0600

    test that checks that black pieces checks the left and right side of board upon calling board build #43

--- client/src/test/java/client/ClientGameTest.java
+++ client/src/test/java/client/ClientGameTest.java
@@ -38,5 +38,36 @@ class ClientGameTest {
 		
 	}
 	
+	@Test 
+	public void testBoardBuildBlackPiecesLeft() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		ClientGame game =  new ClientGame(1, 0, "other");
+		Method privateMethod = ClientGame.class.getDeclaredMethod("buildBoard");
+		privateMethod.setAccessible(true);
+		char[][] board = (char[][]) privateMethod.invoke(game);
+		
+		//CHECKS LEFT SIDE BLACK PIECES
+		assertEquals(board[3] [0], 'b');
+		assertEquals(board[4] [0], 'b');
+		assertEquals(board[5] [0], 'b');
+		assertEquals(board[6] [0], 'b');
+		assertEquals(board[7] [0], 'b');
+		
+	}
+	@Test 
+	public void testBoardBuildBlackPiecesRight() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		ClientGame game =  new ClientGame(1, 0, "other");
+		Method privateMethod = ClientGame.class.getDeclaredMethod("buildBoard");
+		privateMethod.setAccessible(true);
+		char[][] board = (char[][]) privateMethod.invoke(game);
+		
+		//CHECKS RIGHT SIDE BLACK PIECES
+		assertEquals(board[3] [10], 'b');
+		assertEquals(board[4] [10], 'b');
+		assertEquals(board[5] [10], 'b');
+		assertEquals(board[6] [10], 'b');
+		assertEquals(board[7] [10], 'b');
+		
+	}
+
 
 }
