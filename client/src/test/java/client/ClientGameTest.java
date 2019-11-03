881945e6105f8286e054d9d7a73c5d06f9f80a16
Adan Soto <asoto98@rams.colostate.edu>
2019-11-02 22:22:53 -0600

    added test case for board build to make sure corners are empty and also a check for the king to be at the center.

--- client/src/test/java/client/ClientGameTest.java
+++ client/src/test/java/client/ClientGameTest.java
@@ -1,6 +1,10 @@
 package client;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Test;
 
 class ClientGameTest {
@@ -8,6 +12,31 @@ class ClientGameTest {
 	public void testExample() {
 		assertTrue(true);
 	}
-
+	//TESTS FOR BOARD BUILD
+	@Test 
+	public void testBoardBuildCorners() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		ClientGame game =  new ClientGame(1, 0, "other");
+		Method privateMethod = ClientGame.class.getDeclaredMethod("buildBoard");
+		privateMethod.setAccessible(true);
+		char[][] board = (char[][]) privateMethod.invoke(game);
+		
+		//CHECKS CORNERS
+		assertEquals(board[0][0], 'e');
+		assertEquals(board[0][10], 'e');
+		assertEquals(board[10][0], 'e');
+		assertEquals(board[10][10], 'e');
+	}
+	@Test 
+	public void testBoardBuildKing() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		ClientGame game =  new ClientGame(1, 0, "other");
+		Method privateMethod = ClientGame.class.getDeclaredMethod("buildBoard");
+		privateMethod.setAccessible(true);
+		char[][] board = (char[][]) privateMethod.invoke(game);
+		
+		//CHECKS KING
+		assertEquals(board[5][5], 'k');
+		
+	}
+	
 
 }
