d266d536270a1adcd64455a2c4b9c35fa61d41de
Adan Soto <asoto98@rams.colostate.edu>
2019-12-04 10:52:22 -0700

    #64 added constructor for client handler

--- client/src/main/java/client/clientCommunicationHandler.java
+++ client/src/main/java/client/clientCommunicationHandler.java
@@ -1,5 +1,20 @@
 package client;
 
-public class clientCommunicationHandler {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
 
+public class clientCommunicationHandler {
+	 DataInputStream input;
+	 DataOutputStream output;
+	 Socket socket;
+	      
+	  
+	 // Constructor 
+	 public clientCommunicationHandler(Socket socket, DataInputStream input, DataOutputStream output){ 
+	        this.socket = socket; 
+	        this.input = input; 
+	        this.output = output; 
+	 } 
+	  
 }
