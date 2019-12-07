1cb4ca2966f68e2c1fa6d75dbca505fccecbfcec
Adan Soto <asoto98@rams.colostate.edu>
2019-12-07 12:11:18 -0700

    #64 switch statement to act on specific messages

--- client/src/main/java/client/clientCommunicationHandler.java
+++ client/src/main/java/client/clientCommunicationHandler.java
@@ -2,19 +2,56 @@ package client;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 
 public class clientCommunicationHandler {
 	 DataInputStream input;
 	 DataOutputStream output;
+	 int gameID;
 	 Socket socket;
 	      
 	  
 	 // Constructor 
-	 public clientCommunicationHandler(Socket socket, DataInputStream input, DataOutputStream output){ 
+	 public clientCommunicationHandler(Socket socket, DataInputStream input, DataOutputStream output, int gameID){ 
 	        this.socket = socket; 
 	        this.input = input; 
-	        this.output = output; 
+	        this.output = output;
+	        this.gameID = gameID;
 	 } 
+	 
+	 public void actOnMessage() {
+		 String received = null;
+		try {
+			received = input.readUTF();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		 
+		 switch (received) { 
+         
+         case "GameUpdate": 
+            //unpack message to get from and to coordinates
+        	 
+             break; 
+               
+         case "Register": 
+            
+             break; 
+         case "Login": 
+             
+             break;       
+         
+         default: 
+        	 try {
+				output.writeUTF("Invalid action for handler to act on");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+             break; 
+		 } 
+	 }
 	  
 }
