686a58b2188e55ff99e3afdebe0fa7cd8938ec3e
ColinK30 <colin_kitten@yahoo.com>
2019-12-07 13:59:12 -0700

    added maven dependency to pom.xml for jcraft. Added code to tunnel into cs machines.

--- client/src/main/java/client/DBHandler.java
+++ client/src/main/java/client/DBHandler.java
@@ -1,26 +1,64 @@
 package client;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
 public class DBHandler {
 	
 	static final String dbUser   = "cakitten";
 	static final String dbPasswd = "83115757";
-    static final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     private Connection conn;
+    private Session session;
 
     // TODO: figure out connecting to DB
 	public DBHandler() {
+        final String keypath = "~/.ssh/id_rsa";
+        final int port = 22;
+        String host = "denver.cs.colostate.edu";
+//	    String host = "129.82.44.141";
 
         try {
-            Class.forName(JDBC_DRIVER);
-            String url = "jdbc:mariadb://faure/" + dbUser + "?serverTimezone=UTC";
+            JSch jsch = new JSch();
+            jsch.addIdentity(keypath);
+            jsch.setKnownHosts("~/.ssh/known_hosts");
+            session = jsch.getSession(dbUser, host, port);
+            session.setConfig("PreferredAuthentications", "publickey,keyboard-interactive,password");
+            java.util.Properties config = new java.util.Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+            session.connect();
+            session.setPortForwardingL(12345, "localhost", 54321);
+System.out.println(session.isConnected());
+            System.out.println(session.getPortForwardingL()[0]);
+            System.out.println("IP of my system is := "+session.getHost());
 
+        }
+        catch (JSchException e) {
+            throw new RuntimeException("Failed to create Jsch Session object.", e);
+        }
+        dbConnect();
+//        System.out.println("DB Connected");
+	}
+
+	private void dbConnect(){
+        try {
+
+            Class.forName(JDBC_DRIVER);
+            String url = "jdbc:mysql://faure/" + dbUser + "?serverTimezone=UTC";
+
+            System.out.println("before conn");
             conn = DriverManager.getConnection(url, dbUser, dbPasswd);
+            System.out.println("after conn");
 
             System.out.println("URL: " + url);
             System.out.println("Connection: " + conn);
@@ -29,7 +67,7 @@ public class DBHandler {
             e.printStackTrace();
             System.exit(1);
         }
-	}
+    }
 
     // addUser will return true if user was successfully removed
     // false otherwise.
@@ -94,5 +132,10 @@ public class DBHandler {
         }
         return false;
 	}
+
+	public static void main(String args[]){
+	    DBHandler db = new DBHandler();
+	    db.addUser("cakitten", "pass","colin@email.com");
+    }
 
 }
